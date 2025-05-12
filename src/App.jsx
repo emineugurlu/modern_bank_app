2fe7dc912d2444d97a0b7f3e4fab58731f3b9274
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-12 19:31:35 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import styles from './styles'
+import styles from './style';
+
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <h1 >Hello, World!</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+        Navbar
+        </div>
+      </div>
+      <div className={``}>
+
+      </div>
     </div>
   );
 };
