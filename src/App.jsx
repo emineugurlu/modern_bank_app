e17531b5958122408d51ff531f08137e7dbbd3de
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-14 14:58:21 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
+import { Business } from "./components";
 import styles from "./style";
-
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
 const App = () => {
   return (
     <div className="bg-primary w-full min-h-screen overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          Navbar
-        </div>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          Hero Stats Business Billing CardDeal Testimonials Clients CTA Footer
         </div>
       </div>
     </div>
