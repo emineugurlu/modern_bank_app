6a6728dd3aa9ca7c3c7e6506a6a38a69ea9584e7
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-14 15:06:09 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -15,12 +15,23 @@ const App = () => {
   return (
     <div className="bg-primary w-full min-h-screen overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero Stats Business Billing CardDeal Testimonials Clients CTA Footer
+          <div className={`${styles.boxWidth}`}>
+            <Hero />{" "}
+          </div>
+          <Stats />
+          <Business /> <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
