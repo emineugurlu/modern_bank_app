6aab06fbee08f7f7a225e161d146f811b28ae51d
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-14 17:27:38 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,11 @@ import {close, logo,menu} from '../assets'
 import { navLinks } from '../constants'
 const Navbar = () => {
   return (
-   <nav className="w-full flex py-6 justify-between items-center "></nav>
+   <nav className="w-full flex py-6 justify-between items-center ">
+    <img  src ={logo} alt="hookbank"
+    className="w-[124px] h-[32px]"/>
+    <ul className="list-none sm:flex hidden justify-end items-center flex-1"></ul>
+   </nav>
   )
 }
 
