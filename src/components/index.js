833536e0ebaa1fb76faafcb784e8fc3abc21c5a5
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-14 16:05:31 +0300

    update

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Business from "./Business";
-import Clients from "./Client";
+import Clients from "./Clients";
 import CTA from "./CTA";
 import Stats from "./Stats";
 import Footer from "./Footer";
