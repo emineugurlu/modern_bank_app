16d92a52e1d9ae8027d3a39cc4dc804a496de7d2
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-14 14:51:46 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import styles from './style';
-
+import styles from "./style";
 
 const App = () => {
   return (
-    <div className="bg-primary w-full overflow-hidden">
+    <div className="bg-primary w-full min-h-screen overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-        Navbar
+          Navbar
         </div>
       </div>
-      <div className={``}>
 
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+          Stats
+          Business
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
       </div>
     </div>
   );
