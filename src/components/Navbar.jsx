d9b0196d38c70a90fddd25458b2d21ed9b362633
Emine Uğurlu <emineugurlu957@gmail.com>
2025-05-16 14:53:42 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,19 @@
-import { logo } from "../assets";
+import React, { useState } from "react";
+import { logo, menu, close } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div className="w-full bg-primary py-4 px-6 flex items-center justify-between">
       <img src={logo} alt="Logo" className="w-[124px] h-[32px]" />
 
-      <ul className="flex flex-row gap-6 text-white">
+      <ul className="sm:flex hidden flex-row gap-6 text-white">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${
               index === navLinks.length - 1 ? "mr-0" : "mr-10"
             }`}
           >
@@ -18,6 +21,16 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      {/* Hamburger Menü */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
+          onClick={() => setToggle(!toggle)}
+        />
+      </div>
     </div>
   );
 };
